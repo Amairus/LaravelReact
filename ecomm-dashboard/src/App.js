4b25dc77849ea0ce406e741da862d551104eda98
Amarius <a.alla@wisch4web.de>
2022-06-02 16:16:09 +0200

    React Installed

---
+++ ecomm-dashboard/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component }  from 'react';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bobi M</h1>
+      <button>Simple Button</button>
+      <Button>Bootstrap Button</Button>
+    </div>
+  );
+}
+
+export default App;
